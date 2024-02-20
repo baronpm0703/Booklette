ecfab0a6ed90a77897c25f5167cd208a5598cb09
Vo Chanh Tin <tin.vo.4767@outlook.com>
2024-02-21 00:46:46 +0700

    finish create new password UI

--- app/src/main/java/com/example/booklette/VerifyEmailActivity.kt
+++ app/src/main/java/com/example/booklette/VerifyEmailActivity.kt
@@ -1,11 +1,22 @@
 package com.example.booklette
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.booklette.databinding.ActivityMainBinding
+import com.example.booklette.databinding.ActivityVerifyEmailBinding
 
 class VerifyEmailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityVerifyEmailBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_verify_email)
+        binding = ActivityVerifyEmailBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.btnVerifyOTPSubmit.setOnClickListener({
+            startActivity(Intent(this@VerifyEmailActivity, CreateNewPasswordActivity::class.java))
+        })
     }
 }
