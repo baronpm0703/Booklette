ef21fc7e5aae57a126e4d8c62b2420d48f4043f6
Loi-Tap-Lam-Dev <npminhbaobao2@gmail.com>
2024-03-03 15:31:29 +0700

    Update data script

--- external/db_init.sql
+++ external/db_init.sql
@@ -110,13 +110,21 @@ Insert into booklette.Account Values
 	('0HR9Q9Y1HJQvm59UoL4UiJQFXIJ2', 'Nguyễn Phú Minh Bảo', '0948361174','npmbao21@clc.fitus.edu.vn', '22 hẻm 182 Lý Thường Kiệt', '' );
     
 Insert into  booklette.Book Values
-	('01', 'Tuesday Mooney Talks to Ghosts', 'Tiểu Thuyết','Kate Racculia', '1996-12-01', ''),
-    ('02', 'The Catcher in the eyes', 'Tiểu Thuyết','J. D. Salinger', '2003-02-01', ''),
-    ('03', 'Great Expectations', 'Self-Help','Charles Dickens', '2008-11-11', ''),
-    ('04', 'Tôi Thấy Hoa Vàng Trên Cỏ Xanh', 'Tiểu thuyết','Nguyễn Nhật Ánh', '2008-06-11-', ''),
-    ('05', 'One-Punch Man', 'Truyện Tranh','Yusuke Murata, ONE', '2009-03-12', '');
+	(1, 'Tuesday Mooney Talks to Ghosts', 'Novel','Kate Racculia', '1996-12-01', 'https://firebasestorage.googleapis.com/v0/b/book-store-3ed32.appspot.com/o/Books%2FTuesday.jpg?alt=media&token=6a578daf-af25-434e-9952-ffa673a37d62'),
+    (2, 'The Catcher in the eyes', 'Novel','J. D. Salinger', '2003-02-01', 'https://firebasestorage.googleapis.com/v0/b/book-store-3ed32.appspot.com/o/Books%2FCatcher.jpg?alt=media&token=dd8c6fab-4be1-495a-9b07-fe411e61718b'),
+    (3, 'Great Expectations', 'Self-Help','Charles Dickens', '2008-11-11', 'https://firebasestorage.googleapis.com/v0/b/book-store-3ed32.appspot.com/o/Books%2FGreat.jpg?alt=media&token=e7e4ca9f-b789-4bed-b251-5278a821c99d'),
+    (4, 'Tôi Thấy Hoa Vàng Trên Cỏ Xanh', 'Novel','Nguyễn Nhật Ánh', '2008-06-11-', 'https://firebasestorage.googleapis.com/v0/b/book-store-3ed32.appspot.com/o/Books%2FT%C3%B4i_th%E1%BA%A5y_hoa_v%C3%A0ng_tr%C3%AAn_c%E1%BB%8F_xanh.jpg?alt=media&token=ef1fa26e-b44e-40c9-8656-288d16882401'),
+    (5, 'One-Punch Man', 'Comic','Yusuke Murata, ONE', '2009-03-12', 'https://firebasestorage.googleapis.com/v0/b/book-store-3ed32.appspot.com/o/Books%2FOne%20punch%20man.jpg?alt=media&token=6094b6a0-1f73-42a1-81cc-0f0716e92e50'),
+    (6, "The Silence of the Lambs", "Horror", "Thomas Harris", "1988-01-03", "https://firebasestorage.googleapis.com/v0/b/book-store-3ed32.appspot.com/o/Books%2FSilence.jpg?alt=media&token=bebbee82-c995-43d0-a617-ab752582127b"),
+    (7, "Tết Ở Làng Địa Ngục", "Horror", "Thảo Trang", "2022-07-21", "https://firebasestorage.googleapis.com/v0/b/book-store-3ed32.appspot.com/o/Books%2FT%E1%BA%BFt.png?alt=media&token=738a1214-79a0-4db6-9c32-3f1256486dbc"),
+    (8, "Dune", "Fantasy", "Frank Herbert", "1965-08-12", "https://firebasestorage.googleapis.com/v0/b/book-store-3ed32.appspot.com/o/Books%2FDune.jpg?alt=media&token=a666bd06-4600-4762-be03-a367f72be0a4");
     
-    
+Insert into booklette.review values
+	(1, "0HR9Q9Y1HJQvm59UoL4UiJQFXIJ2", 5, "An absolute masterpiece of horror literature! 'Whispers in the Shadows' captivates from the first page, weaving a chilling narrative that keeps you on the edge of your seat. The author's command over suspense and the art of slowly revealing the horror lurking within is truly commendable. A must-read for any horror aficionado!", ""),
+    (2, "NSKxVRDePid6gmNndNGeEmAjsvS2", 4, " 'Eclipsed Nightmare' is a gripping journey into the macabre, blending psychological horror with supernatural elements. The character development is solid, and the author skillfully builds tension throughout. While the ending left me wanting a bit more closure, the overall experience was spine-tingling and memorable.", ""),
+    (1, "NSKxVRDePid6gmNndNGeEmAjsvS2", 3, " 'Crimson Veil' offers a unique take on the horror genre, introducing a captivating plot with unexpected twists. However, the pacing felt uneven at times, and some of the character motivations seemed unclear. While it had moments of genuine fright, it lacked the consistency to secure a higher rating.", ""),
+	(5, "t4TBmyNG4hgrUWyM2eNFEYlAuFu2", 2, "Unfortunately, 'Spectral Echoes' fell short of expectations. The plot seemed convoluted, and the attempts at horror felt forced. The characters lacked depth, making it challenging to connect with their struggles. Overall, the execution failed to deliver the spine-chilling experience promised, resulting in a disappointing read.", ""),
+    (3, "Wanlg9TtCkdvThrmCwheIeMlSHI3", 1, " 'Labyrinth of Despair' is a horror novel that misses the mark entirely. The narrative lacks coherence, and the attempt at creating fear feels more like a series of disconnected scenes rather than a cohesive story. The characters are one-dimensional, and the predictable plot twists make this a forgettable entry into the horror genre.", "");
     
     
     
