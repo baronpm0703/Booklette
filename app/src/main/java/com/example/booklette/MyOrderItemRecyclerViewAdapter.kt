307f2a5f3548acb4e5893bd5078a90ecb295bfa1
Kizark <kiznlh@gmail.com>
2024-04-11 01:14:38 +0700

    fix bug adapter

--- app/src/main/java/com/example/booklette/MyOrderItemRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/booklette/MyOrderItemRecyclerViewAdapter.kt
@@ -38,11 +38,11 @@ class MyOrderItemRecyclerViewAdapter(
     override fun getItemCount(): Int = values.size
 
     inner class ViewHolder(binding: MyOrderItemBinding) : RecyclerView.ViewHolder(binding.root) {
-        val idView: TextView = binding.itemNumber
-        val contentView: TextView = binding.content
+//        val idView: TextView = binding.itemNumber
+//        val contentView: TextView = binding.content
 
         override fun toString(): String {
-            return super.toString() + " '" + contentView.text + "'"
+            //return super.toString() + " '" + contentView.text + "'"
         }
     }
 
